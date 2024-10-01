4bc46005314e41f2cb51c8b3bed9beb8691786c3
Romain Bide <bide.romain@outlook.fr>
2024-10-01 21:23:16 +0200

    Feat: Add the first modal for delete picture

---
+++ FrontEnd/modale.js
@@ -0,0 +1,30 @@
+// Modale pour supprimer une photo
+
+let modaleOverlay = document.querySelector('.modal-container');
+const overlay = document.querySelector('.overlay');
+const cross = document.querySelector('.cross > i');
+
+// Initialiser l'état du style (ne pas réaffecter modaleOverlay)
+modaleOverlay.style.display = "none";
+
+// Faire apparaitre la modale
+const editModal = document.querySelector(".edit-modale");
+
+editModal.addEventListener("click", function(event) {
+    event.preventDefault();
+    modaleOverlay.style.display = "flex"; // Affiche la modale
+    console.log("edit");
+});
+
+// Faire disparaitre la modale
+overlay.addEventListener('click', function(event) {
+    event.preventDefault();
+    modaleOverlay.style.display = "none"; // Cache la modale
+    console.log("clic");
+});
+
+cross.addEventListener('click', function(event) {
+    event.preventDefault();
+    modaleOverlay.style.display = "none"; // Cache la modale
+    console.log("clic");
+});
